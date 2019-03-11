3f43a039ecb1fc6d3016a8fa92a83b481243bae3
Giovanni Gaspard <giovannigaspard@gmail.com>
2019-03-11 10:21:15 -0400

    Matt Magic(Added Better Movement,House)

---
+++ Assets/Scripts/triggerevent2.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerevent2 : MonoBehaviour
+{
+    string scene;
+    void OnCollisionEnter(Collision col) => Initiate.Fade("Scene1", Color.black, 0.5f);
+}
