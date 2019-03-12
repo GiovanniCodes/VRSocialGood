370bfd51deb3314be5ea445c6ed0ea82e38fbe74
Ryan Feeney <ryanq.feeney@gmail.com>
2019-03-12 13:27:23 -0400

    Revert "Grocery"
    This reverts commit 811ed5403be3f24ec64fe477dc906aae6281d659.

--- Scripts/triggerevent2.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class triggerevent2 : MonoBehaviour
-{
-    string scene;
-    void OnCollisionEnter(Collision col) => Initiate.Fade("Scene1", Color.black, 0.5f);
-}
--- Scripts/triggerevent3.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class triggerevent3 : MonoBehaviour
-{
-    string scene;
-    void OnCollisionEnter(Collision col) => Initiate.Fade("Endcredits", Color.black, 0.5f);
-}
