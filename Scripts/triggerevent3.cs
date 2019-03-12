811ed5403be3f24ec64fe477dc906aae6281d659
Ryan Feeney <ryanq.feeney@gmail.com>
2019-03-12 13:18:14 -0400

    Grocery
    first commit doesnt have large assets

---
+++ Scripts/triggerevent3.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerevent3 : MonoBehaviour
+{
+    string scene;
+    void OnCollisionEnter(Collision col) => Initiate.Fade("Endcredits", Color.black, 0.5f);
+}
