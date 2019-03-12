46a888704b9c267995d98b1fc38509fedef11d7a
MatthewHennig <matthewhennig@yahoo.com>
2019-03-12 11:23:36 -0400

    clean up

--- Assets/Scripts/triggerevent1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class triggerevent1 : MonoBehaviour
-{
-    string scene;
-    void OnCollisionEnter(Collision col) => Initiate.Fade("Scene3", Color.black, 0.5f);
-}
