270080d7723831d848bb762f1af5c49939470e8e
MatthewHennig <matthewhennig@yahoo.com>
2019-03-12 11:46:18 -0400

    scene transition set up

---
+++ Assets/_Scripts/SceneCtrl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneCtrl : MonoBehaviour
+{
+      public void changeScene(string scene)
+    {
+        Initiate.Fade(scene, Color.black, 0.5f);
+    }
+
+
+}
