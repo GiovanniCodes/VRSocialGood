33f81de92180a4df6d6cc709a505e9feffa84664
Giovanni Gaspard <giovannigaspard@gmail.com>
2019-03-16 12:50:11 -0400

    Redo

---
+++ Assets/_Scripts/triggerevent.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerevent : MonoBehaviour
+{
+    string scene;
+    void OnCollisionEnter(Collision col) => Initiate.Fade("Market", Color.black, 0.5f);
+}
