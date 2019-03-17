2a6ab1254328d175411c714ee0f4d9908c2f3f56
Giovanni Gaspard <giovannigaspard@gmail.com>
2019-03-16 21:57:19 -0400

    text follower

---
+++ Assets/_Scripts/onHand.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onHand : MonoBehaviour
+{
+   public Transform onHand1;
+
+void OnMouseDown() {
+ GetComponent<Rigidbody>().useGravity = false;
+ this.transform.position = onHand1.transform.position;
+ this.transform.parent = GameObject.Find("FirstPerson").transform;
+}
+}
